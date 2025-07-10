0219fcdaa0e2c8166196f2c3c050fdf46a324c50
Ertan Kayalar <ertan@ertankayalar.com>
2025-07-10 10:21:10 +0300

    yay-cachy

--- 01-yay.sh
+++ 01-yay.sh
@@ -1,10 +1,4 @@
 # Ensure yay (AUR helper) is installed
 if ! command -v yay &>/dev/null; then
-  echo "yay not found. Installing yay (AUR helper)..."
-  sudo pacman -S --needed git base-devel
-  git clone https://aur.archlinux.org/yay.git
-  cd yay
-  makepkg -si --noconfirm
-  cd ..
-  rm -rf yay
+  sudo pacman -S --needed yay
 fi
