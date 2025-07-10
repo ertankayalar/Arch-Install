e0249bdca11bd314448c12977eca852d99a3764e
Ertan Kayalar <ertan@ertankayalar.com>
2025-07-10 11:21:58 +0300

    xtras

--- 20-xtras.sh
+++ 20-xtras.sh
@@ -2,8 +2,6 @@ echo "Installing Xtras AUR packages..."
 yay -S --noconfirm --neded \
   spotify \
   signal-desktop \
-  lazydocker \
-  lazygit \
   google-chrome \
   anydesk-bin \
   dropbox-cli \
