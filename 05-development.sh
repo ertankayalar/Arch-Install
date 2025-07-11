14f8f21940c8c4d0d8fda23b45e6ba829f706915
Ertan Kayalar <ertan@ertankayalar.com>
2025-07-11 17:54:31 +0300

    development

--- 05-development.sh
+++ 05-development.sh
@@ -27,17 +27,6 @@ mise settings set ruby.ruby_build_opts "CC=gcc-14 CXX=g++-14"
 # Trust .ruby-version
 mise settings add idiomatic_version_file_enable_tools ruby
 
-if ! command -v nvim &>/dev/null; then
-  yay -S --noconfirm --needed nvim luarocks tree-sitter-cli
-
-  # Install LazyVim
-  # rm -rf ~/.config/nvim
-  git clone https://github.com/LazyVim/starter ~/.config/nvim
-  # cp -R ~/.local/share/omarchy/config/nvim/* ~/.config/nvim/
-  # rm -rf ~/.config/nvim/.git
-  # echo "vim.opt.relativenumber = false" >>~/.config/nvim/lua/config/options.lua
-fi
-
 mise install nodejs@latest
 mise install npm@latest
 mise install hugo@latest
@@ -47,5 +36,3 @@ mise use nodejs@latest
 mise use npm@latest
 mise use hugo@latest
 mise use ruby@latest
-
-yay -S visual-studio-code-bin
