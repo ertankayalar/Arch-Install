6d9eb66a11a35938cb54e6cc22fd0f2495b1b70d
Ertan Kayalar <ertan@ertankayalar.com>
2025-07-12 08:13:13 +0300

    libyaml

--- 05-development.sh
+++ 05-development.sh
@@ -20,6 +20,10 @@ if ! grep -Fxq "$line" "$HOME/.config/fish/config.fish" 2>/dev/null; then
   echo "$line" >>"$HOME/.config/fish/config.fish"
 fi
 
+yay -S less whois plocate
+
+pacman -S libyaml
+
 # Install Ruby using gcc-14 for compatibility
 yay -S --noconfirm --needed gcc14
 mise settings set ruby.ruby_build_opts "CC=gcc-14 CXX=g++-14"
